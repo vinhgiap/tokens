7ff7036953db55259a0755e0f0d468f36e1ce679
CPE07.vinhgv <CPE07.vinhgv@techcombank.com.vn>
2023-09-15 15:39:37 +0700

    remove all

---
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -0,0 +1,27 @@
+
+//
+// StyleDictionary+Class.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 15 Sep 2023 07:35:16 GMT
+
+
+import UIKit
+
+public class StyleDictionaryClass {
+    public static let dark_bg = #101010
+    public static let dark_button_text = #ffffff
+    public static let dark_error = #ff4a4a
+    public static let dark_main = #0c6dff
+    public static let dark_outline = #282828
+    public static let dark_text_1 = #efefef
+    public static let dark_text_2 = rgba(199,199,199,0.38)
+    public static let light_bg = #ffffff
+    public static let light_button_text = #ffffff
+    public static let light_error = #ff4a4a
+    public static let light_main = #0c6dff
+    public static let light_outline = #d1d1d1
+    public static let light_text_1 = #2f2f2f
+    public static let light_text_2 = #2f2f2f61
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,27 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 15 Sep 2023 07:35:16 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let dark_bg = #101010
+    internal static let dark_button_text = #ffffff
+    internal static let dark_error = #ff4a4a
+    internal static let dark_main = #0c6dff
+    internal static let dark_outline = #282828
+    internal static let dark_text_1 = #efefef
+    internal static let dark_text_2 = rgba(199,199,199,0.38)
+    internal static let light_bg = #ffffff
+    internal static let light_button_text = #ffffff
+    internal static let light_error = #ff4a4a
+    internal static let light_main = #0c6dff
+    internal static let light_outline = #d1d1d1
+    internal static let light_text_1 = #2f2f2f
+    internal static let light_text_2 = #2f2f2f61
+}
